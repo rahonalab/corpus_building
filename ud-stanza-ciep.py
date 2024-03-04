4d3a585e07c9622eecc18c1f13f4e1054698fec9
Luigi <luigi@rahona.be>
2024-03-04 16:21:09 +0100

    sq

--- ud-stanza-ciep.py
+++ ud-stanza-ciep.py
@@ -79,7 +79,7 @@ def main():
     import platform
     '''Prepare config for the NLP pipeline'''
     config = preparenlpconf(ud,args.processors)
-    nlp = stanza.Pipeline(processors=args.processors, allow_unknown_language=True, **config)
+    nlp = stanza.Pipeline(processors=args.processors, **config)
     for filename in sorted(glob.glob(args.source+'/*.txt')):
         file_content = open(filename, encoding='utf-8').read()
         print("Reading: "+filename)
