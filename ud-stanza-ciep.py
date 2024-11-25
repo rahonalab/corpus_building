601a713b9e81bd95871ab423d5a8d62d0e410268
Luigi <luigi@rahona.be>
2024-11-25 19:23:38 +0100

    added pysbd

--- ud-stanza-ciep.py
+++ ud-stanza-ciep.py
@@ -143,8 +143,7 @@ def main():
             nlp = stanza.Pipeline(**config, logging_level="DEBUG", allow_unknown_language=True, use_gpu=gpu, tokenize_no_ssplit=True)
             #Alternate sentence splitting
             file_content = sentPysbd(ud,file_content)
-
-        print(file_content)
+            print(file_content)
         parseciep(nlp, file_content, filename, args.target, args.miniciep)
     print("--- %s seconds ---" % (time.time() - start_time))
     print("Done! Happy corpus-based typological linguistics!\n")
